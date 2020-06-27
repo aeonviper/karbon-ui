8207a4348a4a43c76ae1f495c4d3899d9673bd46
aeonviper <viperblaster@gmail.com>
2020-06-27 20:46:17 +0700

    Code

--- src/application.vue
+++ src/application.vue
@@ -1,6 +1,16 @@
 <template>
 	<div style="margin:10px;">
-		<h1>Karbon</h1>
+		<h1>Karbon version 1.0</h1>
+		<v-form>
+			<v-text-field
+				v-model="text"
+				label="Text"
+			></v-text-field>
+			<div>{{value}}</div>
+			<div style="text-align:center;">
+				<v-btn large @click="action()">Submit</v-btn>
+			</div>
+		</v-form>
 	</div>
 </template>
 
@@ -14,13 +24,26 @@
 </style>
 
 <script>
+import ajax from "axios";
 export default {
 	data: () => ({
-		name: null
+		text: null,
+		value: null
 	}),
-	computed: {
-	},
+	computed: {},
 	methods: {
+		action() {
+			let $this = this;
+			ajax.post("http://localhost:3000/uppercase", {
+				text: $this.text
+			})
+				.then(response => {
+					$this.value = response.data.text;
+				})
+				.catch(error => {
+					console.log(error.response);
+				});
+		},
 	}
 };
 </script>
